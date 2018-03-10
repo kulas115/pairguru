63c73d44e527f5537417e18f6db13c5e15e40931
Kulas115 <kulas115@gmail.com>
2018-03-10 22:53:54 +0100

    Task 6 - add unit specs for most_comments scope

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -10,5 +10,25 @@ FactoryBot.define do
         create_list :comment, 5, user: user
       end
     end
+    trait :with_one_comment do
+      after(:create) do |user|
+        create_list :comment, 1, user: user
+      end
+    end
+    trait :with_two_comments do
+      after(:create) do |user|
+        create_list :comment, 2, user: user
+      end
+    end
+    trait :with_three_comments do
+      after(:create) do |user|
+        create_list :comment, 3, user: user
+      end
+    end
+    trait :with_old_comments do
+      after(:create) do |user|
+        create_list :comment, 3, user: user, created_at: 10.days.ago
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,39 @@ describe User do
   it { is_expected.not_to allow_value("+48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("aaa +48 aaa bbb ccc").for(:phone_number) }
   it { is_expected.not_to allow_value("+48 999 888 777\naseasd").for(:phone_number) }
+
+
+  describe ".most_comments" do
+    context "returns valid relation containing" do
+      it "exactly 10 users" do
+        create_list(:user, 20, :with_comments)
+        expect(User.most_comments.to_a.count).to eq(10)
+      end
+      it "users with comments not older than 7 days" do
+        user_new_comments = FactoryBot.create(:user, :with_comments)
+        user_old_comments = FactoryBot.create(:user, :with_old_comments)
+
+        expect(User.most_comments).to include(user_new_comments)
+        expect(User.most_comments).to_not include(user_old_comments)
+      end
+      it "users sorted descending by comments count" do
+        user1 = FactoryBot.create(:user, :with_one_comment)
+        user2 = FactoryBot.create(:user, :with_two_comments)
+        user3 = FactoryBot.create(:user, :with_three_comments)
+
+        expect(User.most_comments).to eq [user3, user2, user1]
+        expect(User.most_comments).to_not eq [user1, user3, user2]
+      end
+    end
+    context "returns empty relation" do
+      it "when users got no comments" do
+        create_list(:user, 20)
+        expect(User.most_comments).to be_empty
+      end
+      it "when users comments are older than 7 days" do
+        create_list(:user, 20, :with_old_comments)
+        expect(User.most_comments).to be_empty
+      end
+    end
+  end
 end
