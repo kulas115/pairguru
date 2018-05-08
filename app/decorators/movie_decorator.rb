c596b42a47e5697b37f7d20f34fea12bac637a0b
kulas115 <kulas115@gmail.com>
2018-05-08 13:45:20 +0200

    Add formated released at date to view using decorator

--- app/decorators/movie_decorator.rb
+++ app/decorators/movie_decorator.rb
@@ -6,4 +6,8 @@ class MovieDecorator < Draper::Decorator
       %w(abstract nightlife transport).sample +
       "?a=" + SecureRandom.uuid
   end
+
+  def formated_realeased_at
+    released_at.strftime("%d.%m.%Y")
+  end
 end
